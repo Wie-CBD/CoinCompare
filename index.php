81b2bcbf27bfe612d150c0dfae82f9af1d3dff7f
Wie <wicahya.bagus@gmail.com>
2017-08-27 01:46:05 +1000

    Filtering and sorting system. also logo.

--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <html>   
     
     
-    <head>
-    <!-- Latest compiled and minified CSS -->
+<head>
+<title>BitCompare</title>
+<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
- 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script ></script>
-    <script
-  src="https://code.jquery.com/jquery-3.2.1.js"
-  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
-  crossorigin="anonymous"></script>
-    <?php
-          
-         
-        
+<script  src="https://code.jquery.com/jquery-3.2.1.js"  integrity="sha256 DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="  crossorigin="anonymous"></script>
+    <script type="application/javascript" src="src/js/chosen.jquery.js"></script>
+<link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+<link rel="stylesheet" type="text/css" href="src/css/chosen.css" media="screen" />    
+<?php
             //ask for the JSON files
             ini_set("allow_url_fopen", 1);  
             //ask for the latest data from whattomine
@@ -35,6 +30,8 @@
                         }
             } 
             $btc_price = $bittrex_market_summary["result"][252]["Last"]; 
+            $currencies = $bittrex_currencies["result"];
+            $market = $bittrex_btc_market;
     ?>  
     
     </head>
@@ -43,7 +40,8 @@
     
     <body>
         <div class="jumbotron text-center">
-             <h1>Coins!</h1>
+            <h1><img class="img-responsive center-block" id="header_img" src="img/logo.png">
+            </h1>
             <h3>A simple Crypto Currency Value Checker page</h3>
             <p>Data taken from Bittrex.</p>
             <p>BTC = <?php echo $btc_price; ?> USD</p>
@@ -55,10 +53,9 @@
             <div class="col-md-3">
                     
                     <div class="panel panel-default">
-                   
                     <div class="panel-heading">Coins</div>
                     <div class="panel-body">
-                         <form method="post" name ="sorter" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                         <form method="post" name ="sorter" action="<?php echo $_SERVER['PHP_SELF'];?>"> 
                         <label>Sort By</label>
                         <select name="sortType">
                             <option value="lastPrice" <?php if(isset($_POST['sortType'])&&($_POST['sortType']=="lastPrice")){ echo "selected"; }  ?>>Last Price</option>
@@ -73,9 +70,27 @@
                                     
                                     ?>>Volume</option> 
                              </select> 
-                        <input type="submit" name="submit" class="btn btn-primary" role="button">
-                        </form>
-                        </div>
+                        
+                        <label>Show Only...</label>
+                        <select multiple name="coinFilters[]" class="chosen-select"> 
+                            <?php
+                            foreach($bittrex_market_summary['result'] as $coin){
+                            $CoinTag =  str_replace("BTC-","",$coin["MarketName"]);
+                            $CurrencyLong = getCurrencyLong($CoinTag,$currencies);
+                            echo '<option value='.$CoinTag.'>'.$CurrencyLong.'</option>';
+                            }
+                            ?>
+                        </select> 
+                            
+                        <input style="margin-top: 5px;" type="submit" name="submit" class="btn btn-primary" role="button">
+                         
+                        </form> 
+                         
+                        
+                        <button onClick="window.location.reload()" class="btn btn-danger">Reset</button>
+                        
+                        
+                    </div>
                   
                     </div>
                 
@@ -83,8 +98,7 @@
             </div>
             <div class="col-md-9">  
             <?php 
-                $currencies = $bittrex_currencies["result"];
-                $market = $bittrex_btc_market;
+                
                 function sortByPrice($a, $b) {
                     return ($b["Last"]<$a["Last"])?-1:1;
                 }
@@ -94,9 +108,44 @@
                 function sortByVolume($a, $b){
                     return ($b["BaseVolume"]<$a["BaseVolume"])?-1:1;
                 }
-                //sort it by price by default
-                usort($market, 'sortByVolume'); 
-                if(isset($_POST['sortType'])){ 
+                $filteredCoin = array();
+               
+                
+                if(!empty($filteredCoin)){
+                    echo 'Previous Filtered coin detected';
+                    
+                }
+                
+                else if(isset($_POST['coinFilters'])){
+                    $filterCoin = array();
+                    $filterCoin = $_POST['coinFilters']; 
+                    foreach($market as $market_coin){
+                        $coinTag = str_replace("BTC-","",$market_coin["MarketName"]);
+                        foreach($filterCoin as $filter){
+                            if($coinTag == $filter){  
+                                $filteredCoin[] = $market_coin;
+                            }
+                        }
+                    }
+                    if(isset($_POST['sortType'])){ 
+                        if($_POST['sortType'] == 'name'){
+                            usort($filteredCoin, 'sortByName');
+                        }
+                        if($_POST['sortType'] == 'lastPrice'){
+                            usort($filteredCoin, 'sortByPrice');
+                        }
+                        if($_POST['sortType'] == 'volume'){
+                            usort($filteredCoin, 'sortByVolume');
+                        }
+                    }
+                    else{
+                        usort($filteredCoin, 'sortByVolume');
+                    }
+                    rowCreator($filteredCoin,$currencies, $btc_price);
+                    
+                }
+                else{
+                 if(isset($_POST['sortType'])){ 
                     if($_POST['sortType'] == 'name'){
                         usort($market, 'sortByName');
                     }
@@ -107,19 +156,25 @@
                         usort($market, 'sortByVolume');
                     }
                 }     
-                rowCreator($market,$currencies, $btc_price);
+                else{//sort it by price by default
+                usort($market, 'sortByVolume'); 
+                }
+                    rowCreator($market,$currencies, $btc_price);
+                }
                 
             ?> 
                 </div>
             </div>
         </div>
     </body>
-    <script>  
+    <script>
+        $(function(){
+             $(".chosen-select").chosen({no_results_text: "Oops, nothing found!"}); 
+        })
+         
     </script>
     
 <?php  
-    
-
 function rowCreator($coins, $names, $price){
     $numberOfRows = ceil(sizeof($coins)/3);
     $lastIndex = 0;
@@ -134,7 +189,6 @@ function rowCreator($coins, $names, $price){
                 echo "</div>";
     }
 }
-    
 function panelCreator($coin, $names, $price){ 
     $coinTag = str_replace("BTC-","",$coin["MarketName"]);
     $coinName = getCurrencyLong($coinTag, $names);
@@ -175,17 +229,13 @@ function panelCreator($coin, $names, $price){
                 '; 
     }
 } 
-
 function getCurrencyLong($tag,$array){
                 foreach($array as $key => $val){
                     if($val["Currency"] == $tag){
                         return $val["CurrencyLong"];
                     }
                 }
-}
-
-
-    
+}    
 ?>
 
 
